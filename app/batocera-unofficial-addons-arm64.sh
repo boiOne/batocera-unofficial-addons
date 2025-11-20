d7d06019bef5298ec9df44eb1262ead7527bc892
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-11-20 03:19:19 +0000

    Update push

--- app/batocera-unofficial-addons-arm64.sh
+++ app/batocera-unofficial-addons-arm64.sh
@@ -6,8 +6,12 @@ install_bua() {
     curl -L install.batoaddons.app | bash
 }
 
-if grep -q 'DTJW92' "$LAUNCH_PATH"; then
+if grep -q 'DIALOGRC' "$LAUNCH_PATH"; then
+    rm -f /userdata/roms/ports/bua.sh.keys
     install_bua
+    dialog --title "UPDATED" --msgbox \
+		"BUA has been updated! A whole new look! Please report any issues to the discord!\n\nJust reopen the app to take a look..." 10 60
+    exit 0
 fi
 
 # Function to display animated title with colors
