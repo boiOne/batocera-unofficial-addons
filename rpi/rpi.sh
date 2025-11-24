872975e41e82b389f23788c81aa2a7f3a304043a
TheNotoriousFOX <d.weatherall92@gmail.com>
2025-11-24 21:16:36 +0000

    Update rpi.sh

--- rpi/rpi.sh
+++ rpi/rpi.sh
@@ -27,11 +27,11 @@ arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
     echo "Architecture: x86_64 detected."
-    appimage_url=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r ".assets[] | select(.name | endswith(\"$AMD_SUFFIX\")) | .browser_download_url")
+    appimage_url=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r ".assets[]? | select(.name | endswith(\"$AMD_SUFFIX\")) | .browser_download_url" | head -n1)
 elif [ "$arch" == "aarch64" ]; then
     echo "Architecture: arm64 detected."
     if [ -n "$ARM_SUFFIX" ]; then
-        appimage_url=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r ".assets[] | select(.name | endswith(\"$ARM_SUFFIX\")) | .browser_download_url")
+        appimage_url=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r ".assets[]? | select(.name | endswith(\"$ARM_SUFFIX\")) | .browser_download_url" | head -n1)
     else
         echo "No ARM64 AppImage suffix provided. Skipping download. Exiting."
         exit 1
